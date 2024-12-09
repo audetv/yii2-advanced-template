c4cf2973762cc171d3510798114b2807dd5ec066
agusev <audetv@gmail.com>
2024-12-09 17:27:58 +0300

    Удалены неиспользуемые экшены

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -40,7 +40,7 @@ AppAsset::register($this);
         ['label' => 'Contact', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => 'Signup', 'url' => ['/auth/join/request']];
     }
 
     echo Nav::widget([
@@ -50,7 +50,7 @@ AppAsset::register($this);
     if (Yii::$app->user->isGuest) {
         echo Html::tag('div',Html::a('Login',['/auth/auth/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
     } else {
-        echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
+        echo Html::beginForm(['/auth/auth/logout'], 'post', ['class' => 'd-flex'])
             . Html::submitButton(
                 'Logout (' . Yii::$app->user->identity->getEmail() . ')',
                 ['class' => 'btn btn-link logout text-decoration-none']
